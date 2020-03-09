7801960e5c50f22702e8ef12e68c71097a7c53f4
Archana <Archana.SudhakaranNair@gov.bc.ca>
2020-03-09 15:51:13 -0700

    Hero-tour application from angular

---
+++ app-basics/src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+
+import {Heroes} from '../mock-heroes';
+
+import {Hero} from './hero'
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  
+   heroes = Heroes;
+
+   selectedHero: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
+  constructor() { }
+
+  ngOnInit(): void {
+   
+  }
+
+}
---
+++ app-basics/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './heroes/hero';
+
+export const Heroes: Hero[] = [
+    { id: 11, name: 'Dr Nice',address:'Canada' },
+    { id: 12, name: 'Narco' ,address:'Canada' },
+    { id: 13, name: 'Bombasto',address:'Canada'  },
+    { id: 14, name: 'Celeritas',address:'Canada'  },
+    { id: 15, name: 'Magneta' ,address:'Canada' },
+    { id: 16, name: 'RubberMan',address:'Canada'  },
+    { id: 17, name: 'Dynama',address:'Canada'  },
+    { id: 18, name: 'Dr IQ' ,address:'Canada' },
+    { id: 19, name: 'Magma' ,address:'Canada' },
+    { id: 20, name: 'Tornado',address:'Canada'  }
+  ];
